40b5554104adef39ec4669c47cfbe3bb51b409cb
DinoDesteney <desbickel@hotmail.com>
2016-06-24 13:51:56 +0200

    The Wall

--- users.php
+++ users.php
@@ -1,9 +1,11 @@
 
 <head>
     <link rel="stylesheet" type="text/css" href="home.css" />
-    				<link rel="stylesheet" type="text/css" href="css/default.css" />
-		<link rel="stylesheet" type="text/css" href="css/component.css" />
+    <link rel="stylesheet" type="text/css" href="css/default.css" />
+<link rel="stylesheet" type="text/css" href="css/component.css" />
     <script src="js/modernizr.custom.js"></script>
+    <script src="js/tabPaneel.js"></script>
+    <script src="js/toucheffects.js"></script>
 </head>
 <body>
 <?php
